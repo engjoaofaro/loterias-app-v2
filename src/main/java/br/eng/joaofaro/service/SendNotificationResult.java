dfe791105f06f0dfbbf7bd43dfa493a814717e33
João Faro <projetos@joaofaro.eng.br>
2023-04-01 14:19:05 -0300

    add sqs notification

--- src/main/java/br/eng/joaofaro/service/SendNotificationResult.java
+++ src/main/java/br/eng/joaofaro/service/SendNotificationResult.java
@@ -1,10 +1,29 @@
 package br.eng.joaofaro.service;
 
 import br.eng.joaofaro.GameDto;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class SendNotificationResult {
 
     public void send(GameDto dto) {
+        GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
+        String lottery = gson.toJson(dto);
+        AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withRegion(Regions.SA_EAST_1)
+                .build();
+        SendMessageRequest request = new SendMessageRequest()
+                .withQueueUrl(System.getenv("QUEUE_URL"))
+                .withMessageBody(lottery)
+                .withDelaySeconds(30);
+        sqs.sendMessage(request);
         System.out.println("Aguarde resultado por e-mail. Obrigado");
         System.out.println();
     }
